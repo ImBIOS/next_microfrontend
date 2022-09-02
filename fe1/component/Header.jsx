41d74f3e510b2f70d77e09264a476da2e435dded
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-09-02 15:42:19 +0700

    add Header component

---
+++ fe1/component/Header.jsx
@@ -0,0 +1,22 @@
+const Header = () => {
+  return (
+    <div
+      style={{
+        background: "black",
+        width: "100%",
+        height: "100px",
+        color: "white",
+        textAlign: "center",
+        display: "flex",
+        justifyContent: "left",
+        alignItems: "center",
+        fontSize: "24px",
+        paddingLeft: "15px",
+      }}
+    >
+      Name
+    </div>
+  );
+};
+
+export default Header;
