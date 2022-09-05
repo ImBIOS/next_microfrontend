dc93741cabaa2586009903bcf74222288a16623d
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2022-09-05 09:00:15 +0700

    add micro fe config in fe1

--- fe1/next.config.js
+++ fe1/next.config.js
@@ -2,6 +2,39 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  webpack5: true, // Need to make it true for some versions of Next JS
+  distDir: "build", // Defined build directory
+  webpack: (config, options) => {
+    // webpack configurations
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        name: "fe1",
+        filename: "remoteEntry.js", // remote file name which will used later
+        remoteType: "var",
+        exposes: {
+          // expose all component here.
+          "./header": "./src/component/Header",
+        },
+        shared: [
+          {
+            react: {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+          {
+            "react-dom": {
+              eager: true,
+              singleton: true,
+              requiredVersion: false,
+            },
+          },
+        ],
+      })
+    );
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
